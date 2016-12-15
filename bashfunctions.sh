1af47a5c0254645ca5b0d94f2b900349ea4a99d3
Paul Chvostek <paul@it.ca>
2016-12-15 01:40:44 -0500

    function collection. May be split out later.

---
+++ bashfunctions.sh
@@ -0,0 +1,37 @@
+
+urlencode() {
+	local LC_COLLATE=C
+	local length="${#1}"
+	for (( i = 0; i < length; i++ )); do
+		local c="${1:i:1}"
+		case "$c" in
+			[A-Za-z0-9.~_-]) printf "$c" ;;
+			*) printf '%%%02X' "'$c" ;;
+		esac
+	done
+}
+urldecode() {
+	local url_encoded="${1//+/ }"
+	printf '%b' "${url_encoded//%/\\x}"
+}
+
+
+function array_contains_old {
+	# options: arrayname value
+	eval 'local values=("${'$1'[@]}")'
+	local element
+	for element in "${values[@]}"; do
+		[[ "$element" == "$2" ]] && return 0
+	done
+	return 1
+}
+
+# Avoid the loop
+function array_contains {
+	# options: arrayname value
+	local -A _arr=()
+	eval _arr=( $(eval printf '[%s]="1"\ ' "\${$1[@]}") )
+	return $(( 1 - 0${_arr[$2]} ))
+}
+
+
